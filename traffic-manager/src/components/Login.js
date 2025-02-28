8eb0b7dff690fab766de5a6b5f46fbc6b7bb8a66
Josh George <joshgeorge987@gmail.com>
2025-02-28 13:40:17 +0530

    login page

--- traffic-manager/src/components/Login.js
+++ traffic-manager/src/components/Login.js
@@ -4,11 +4,13 @@ import "./Login.css";
 const Login = () => {
     return (
         <div className="login-container">
-            <div className="login-box">
-                <input type="email" placeholder="Email" className="input-field" />
-                <input type="password" placeholder="Password" className="input-field" />
-                <button className="login-btn">Log in</button>
-                <a href="/" className="forgot-password">Forgot Password?</a>
+            <div className="split-login">
+                <div className="login-box">
+                    <input type="email" placeholder="Email" className="input-field top" />
+                    <input type="password" placeholder="Password" className="input-field" />
+                    <button className="login-btn">Log in</button>
+                    <a href="/" className="forgot-password">Forgot Password?</a>
+                </div>
             </div>
         </div>
     );
